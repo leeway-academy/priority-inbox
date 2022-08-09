ced167146ed04d571894cd38f92e7f86442dba88
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2022-08-09 09:24:08 +0200

    Fixing command running context

--- run.php
+++ run.php
@@ -6,7 +6,6 @@ require __DIR__ . '/vendor/autoload.php';
 
 use PriorityInbox\Command\ReleaseEmailCommand;
 use Symfony\Component\Console\Application;
-use Symfony\Component\Console\Tester\CommandTester;
 
 if (php_sapi_name() != 'cli') {
     die('This application must be run on the command line.');
@@ -16,18 +15,18 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 $application = new Application();
+$command = new ReleaseEmailCommand();
 
-$releaseEmailCommand = new ReleaseEmailCommand();
+$application->add($command);
+$application->setDefaultCommand($command->getName(), true);
 
-$commandRunner = new CommandTester($releaseEmailCommand);
+$_SERVER['argv'][] = $_ENV['HIDDEN_LABEL_ID'];
+$_SERVER['argv'][] = $_ENV['APPLICATION_NAME'];
+$_SERVER['argv'][] = $_ENV['CLIENT_SECRET_PATH'];
+$_SERVER['argv'][] = $_ENV['CLIENT_CREDENTIALS_PATH'];
 
 try {
-    $commandRunner->execute([
-        "hidden-label-id" => $_ENV['HIDDEN_LABEL_ID'],
-        "application-name" => $_ENV['APPLICATION_NAME'],
-        "client-secret-path" => $_ENV['CLIENT_SECRET_PATH'],
-        "client-credentials-path" => $_ENV['CLIENT_CREDENTIALS_PATH'],
-    ]);
+    $application->run();
 } catch (Exception $e) {
     die($e->getMessage());
 }
