f4e1a455753bc0af929bebd1d82731977872d317
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2022-08-16 15:09:55 +0200

    Fixed email tests

--- test/PriorityInbox/EmailShould.php
+++ test/PriorityInbox/EmailShould.php
@@ -7,16 +7,18 @@ use PHPUnit\Framework\TestCase;
 
 class EmailShould extends TestCase
 {
+    const LABEL_ID = "added";
+
     /**
      * @test
      */
     public function keep_track_of_added_labels(): void
     {
         $email = $this->buildEmail();
-        $newLabel = new Label("added");
+        $newLabel = new Label(self::LABEL_ID);
 
         $email->addLabel($newLabel);
-        $this->assertEquals([$newLabel], $email->addedLabels());
+        $this->assertEquals([self::LABEL_ID => $newLabel], $email->addedLabels());
     }
 
     /**
@@ -25,12 +27,12 @@ class EmailShould extends TestCase
     public function keep_track_of_removed_labels(): void
     {
         $email = $this->buildEmail();
-        $newLabel = new Label("added");
+        $newLabel = new Label(self::LABEL_ID);
 
         $email->addLabel($newLabel);
         $email->removeLabel($newLabel);
 
-        $this->assertEquals([$newLabel], $email->removedLabels());
+        $this->assertEquals([self::LABEL_ID => $newLabel], $email->removedLabels());
     }
 
     /**
