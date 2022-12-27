f2f2d01d6e79428fe4f5bd03eff931b19320e769
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2022-12-27 09:57:51 +0100

    Fixed internet access within container

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-docker run -v $SCRIPT_DIR/app:/app/:rw gmail-priority-inbox php run.php -vvv $@
+docker run -v $SCRIPT_DIR/app:/app/:rw --network=host gmail-priority-inbox php run.php -vvv "$@"
